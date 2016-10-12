e046d6d9e45944f7012f797e95ea888e7e54fbc3
James Spencer <thisjustosay@gmail.com>
2016-10-12 10:41:33 -0700

    initial iOS call hook

--- src/ios/CallHooks.swift
+++ src/ios/CallHooks.swift
@@ -4,19 +4,24 @@ import CoreTelephony
 @objc(CallHooks) class CallHooks : CDVPlugin  {
     func callEnded(_ command: CDVInvokedUrlCommand) {
         
-        
         let callCenter:CTCallCenter = CTCallCenter()
         
+        let timeStart:Double = NSDate().timeIntervalSince1970
+        
         callCenter.callEventHandler = { (call:CTCall!) in
-            
             switch call.callState {
+            
             case CTCallStateConnected:
-                let timeStart = NSDate().timeIntervalSince1970
                 print("CTCallStateConnected")
                 
             case CTCallStateDisconnected:
                 print("CTCallStateDisconnected")
-                let timeEnd = NSDate().timeIntervalSince1970
+                let timeEnd:Double = NSDate().timeIntervalSince1970
+                let json = "{ \"start\":" + String(format:"%f", timeStart) + ", \"end\":" + String(format:"%f", timeEnd) + "}"
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,
+                                                   messageAs: json)
+                self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
+                
             default:
                 break
             }
