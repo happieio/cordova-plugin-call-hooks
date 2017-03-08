daf072f9c76ee1e17397ed118826e11ee7709747
James Spencer <thisjustosay@gmail.com>
2017-03-08 14:29:56 -0800

    support idle and off hook

--- src/ios/CallHooks.swift
+++ src/ios/CallHooks.swift
@@ -10,10 +10,14 @@ import CoreTelephony
             
             case CTCallStateConnected:
                 print("CTCallStateConnected")
-                
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "OFFHOOK")
+                pluginResult?.setKeepCallbackAs(true)
+                self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
+
             case CTCallStateDisconnected:
                 print("CTCallStateDisconnected")
                 let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "IDLE")
+                pluginResult?.setKeepCallbackAs(true)
                 self.commandDelegate!.send(pluginResult, callbackId:command.callbackId)
                 
             default:
